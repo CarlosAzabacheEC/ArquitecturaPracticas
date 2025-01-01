37abb15d30e31a2b4738cf20329fb637f2904016
cazabaches <carlos@azabaches.net>
2025-01-01 03:43:34 -0500

    Refactor using Compose Method

---
+++ ComposeMethod/Lista.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace RefactoringToPatterns.ComposeMethod
+{
+    public class Lista
+    {
+        private readonly bool _readOnly;
+        private int _size;
+        private Object[] _elements;
+
+        public Lista(bool readOnly)
+        {
+            _readOnly = readOnly;
+            _size = 0;
+            _elements = new Object[_size];
+        }
+
+        public void Add(Object element)
+        {
+            if (!_readOnly)
+            {
+                EnsureCapacity();
+                AddElement(element);
+            }
+        }
+
+        private void EnsureCapacity()
+        {
+            int newSize = _size + 1;
+
+            if (newSize > _elements.Length)
+            {
+                Object[] newElements = ExpandElementsArray();
+
+                for (int i = 0; i < _size; i++)
+                    newElements[i] = _elements[i];
+
+                _elements = newElements;
+            }
+        }
+
+        private Object[] ExpandElementsArray()
+        {
+            return new Object[_elements.Length + 10];
+        }
+
+        private void AddElement(Object element)
+        {
+            _elements[_size++] = element;
+        }
+
+        public object[] Elements()
+        {
+            return _elements;
+        }
+    }
+}
